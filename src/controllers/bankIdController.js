e54c34f4d51fc0a7bd9dc7a7c803a55dc855f968
fakhrad <fakhrad@openratio.com>
2019-07-09 11:27:57 +0430

    sign method impelemented

--- src/controllers/bankIdController.js
+++ src/controllers/bankIdController.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-exports.start = function(req, res, next)
+exports.authenticate = function(req, res, next)
 {
     var accessToken = process.env.ACCESS_TOKEN || "1059b83f-72da-47ae-a4f2-5db923545fbd";
     var apiRoot = process.env.API_ROOT || "https://test.zignsec.com/v2"; // for prod set to https://api.zignsec.com/v2
@@ -86,7 +86,44 @@ exports.collect = function(req, res, next)
 
 exports.sign = function(req, res, next)
 {
+  var accessToken = process.env.ACCESS_TOKEN || "1059b83f-72da-47ae-a4f2-5db923545fbd";
+  var apiRoot = process.env.API_ROOT || "https://test.zignsec.com/v2"; // for prod set to https://api.zignsec.com/v2
 
+  var pno = req.body.personalNumber;
+  var config = {
+    url : "/bankidse/Sign",
+    baseURL : apiRoot,
+    method : "post",
+    data : {"PersonalNumber" : pno, "userVisibleData" : req.body.userVisibleData},
+    headers : {
+        'Authorization' : accessToken
+    }
+  };
+  axios(config).then(function (response) {
+      res.status(200).send(response.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        res.status(error.response.status).send(error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+        res.status(204).send("No response from BankID server");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+        res.status(500).send(error.message);
+      }
+      console.log(error.config);
+      res.status(400).send(error.config);
+    });
 }
 
 exports.cancel = function(req, res, next)
