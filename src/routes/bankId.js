048bc2d2269772e434115c97d668e469a207e99e
fakhrad <fakhrad@openratio.com>
2019-07-09 11:36:48 +0430

    start renamed to authenticate

--- src/routes/bankId.js
+++ src/routes/bankId.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/bankIdController');
 
-router.post("/start", controller.start);
+router.post("/start", controller.authenticate);
 router.get("/collect", controller.collect);
 router.post("/cancel", controller.cancel);
 router.post("/sign", controller.sign);
