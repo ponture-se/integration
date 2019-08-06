6550988cef72c6350d38ac2c344c6c98a10e9f8e
fakhrad <fakhrad@openratio.com>
2019-08-06 13:26:00 +0430

    get needs list api added

--- src/controllers/opportunityController.js
+++ src/controllers/opportunityController.js
@@ -72,3 +72,43 @@ exports.submit = function(req, res, next)
       res.status(400).send(error.config);
     });
 }
+
+exports.getNeedsList = function(req, res, next)
+{
+  var accessToken = req.access_token;
+  var apiRoot = process.env.SALESFORCE_API_ROOT || "https://crmdev-ponture-crmdev.cs84.force.com"; // for prod set to https://api.zignsec.com/v2
+  var config = {
+    url : "needs/services/apexrest/getNeedsFields",
+    baseURL : apiRoot,
+    method : "get",
+    params : req.query
+  };
+  console.log(config);
+  axios(config).then(function (response) {
+    console.log(response.data);
+    console.log(response.status);
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        res.status(error.response.status).send(error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+        res.status(204).send("No response from BankID server");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message);
+        res.status(500).send(error.message);
+      }
+      console.log(error.config);
+      res.status(400).send(error.config);
+    });
+}
--- src/routes/opportunity.js
+++ src/routes/opportunity.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var controller = require('../controllers/opportunityController');
 var auth = require('../controllers/auth');
 
+router.get("/needslist", controller.getNeedsList);
 router.get("/companies", auth.verifyToken, auth.getRoaringToken, controller.getCompanies);
 router.post("/submit", auth.verifyToken, auth.getSalesForceToken, controller.submit);
 
