6b2362acf03350a0c16ed0a0ebc68d07949a6274
fakhrad <fakhrad@openratio.com>
2019-08-12 11:42:07 +0430

    some validation added to submit api

--- src/controllers/opportunityController.js
+++ src/controllers/opportunityController.js
@@ -98,155 +98,181 @@ function callRoaring(
     });
 }
 
-exports.submit = function(req, res, next) {
-  var token = req.roaring_access_token;
-  console.log(token);
-  var tasks = {
-    overview: function(callback) {
-      callRoaring(
-        callback,
-        "/se/company/overview/1.1/" + req.body.orgNumber,
-        "get",
-        undefined,
-        "COMPANY_OVERVIEW_INVALID_RESPONSE",
-        "COMPANY_OVERVIEW_API_ERROR",
-        token
-      );
-    },
-    ecoOverview: function(callback) {
-      callRoaring(
-        callback,
-        "/se/company/economy-overview/1.1/" + req.body.orgNumber,
-        "get",
-        undefined,
-        "COMPANY_ECOOVERVIEW_INVALID_RESPONSE",
-        "COMPANY_ECOOVERVIEW_API_ERROR",
-        token
-      );
-    },
-    boardMembers: function(callback) {
-      callRoaring(
-        callback,
-        "/se/company/board-members/1.1/" + req.body.orgNumber,
-        "get",
-        undefined,
-        "COMPANY_BOARDMEMBERS_INVALID_RESPONSE",
-        "COMPANY_BOARDMEMBERS_API_ERROR",
-        token
-      );
-    },
-    beneficialOwners: function(callback) {
-      callRoaring(
-        callback,
-        "/se/beneficialowner/1.0/company/" + req.body.orgNumber,
-        "get",
-        undefined,
-        "COMPANY_BENEFICIAL_INVALID_RESPONSE",
-        "COMPANY_BENEFICIAL_API_ERROR",
-        token
-      );
-    },
-    signatory: function(callback) {
-      callRoaring(
-        callback,
-        "/se/company/signatory/1.1/" + req.body.orgNumber,
-        "get",
-        undefined,
-        "COMPANY_SIGNATORY_INVALID_RESPONSE",
-        "COMPANY_SIGNATORY_API_ERROR",
-        token
-      );
-    },
-    cmpSanctionInfo: function(callback) {
-      callRoaring(
-        callback,
-        "/global/sanctions-lists/1.0/search",
-        "get",
-        {
-          name: req.body.orgName
-        },
-        "COMPANY_SANCTION_INVALID_RESPONSE",
-        "COMPANY_SANCTION_API_ERROR",
-        token
-      );
-    },
-    perSanctionInfo: function(callback) {
-      callRoaring(
-        callback,
-        "/global/sanctions-lists/1.0/search",
-        "get",
-        {
-          name: req.body.bankid.userInfo.name
-        },
-        "PERSON_SACNTION_INVALID_RESPONSE",
-        "PERSON_SACNTION_API_ERROR",
-        token
-      );
-    },
-    pepInfo: function(callback) {
-      callRoaring(
-        callback,
-        "/nordic/pep/1.0/search",
-        "get",
-        {
-          personalNumber: req.body.personalNumber,
-          countryCode: "se"
-        },
-        "PEP_INVALID_RESPONSE",
-        "PEP_API_ERROR",
-        token
-      );
-    }
-  };
-  var roaring;
-  async.parallel(async.reflectAll(tasks), function(errors, results) {
-    console.log(errors);
-    //console.log(results);
-    for (var attr in results) req.body[attr] = results[attr].value;
-    token = req.sf_access_token;
-    console.log(req.body);
-    var apiRoot =
-      process.env.SALESFORCE_API_ROOT || "https://cs85.salesforce.com"; // for prod set to https://api.zignsec.com/v2
-    var config = {
-      url: "/services/apexrest/submitWithoutCallout",
-      baseURL: apiRoot,
-      method: "post",
-      data: req.body,
-      headers: {
-        Authorization: "Bearer " + token
+exports.submit = [
+  // Validate fields
+  check("personalNumber")
+    .isNumeric()
+    .isLength({ min: 12, max: 12 })
+    .withMessage("Personal number is invalid")
+    .matches(/^(19|20)?[0-9]{2}(0|1)[0-9][0-3][0-9][-]?[0-9]{4}$/)
+    .withMessage("Personal number is in invalid format"),
+  check("orgNumber")
+    .isNumeric()
+    .isLength({ min: 9, max: 10 })
+    .withMessage("Organization number is invalid"),
+  check("orgName")
+    .isString()
+    .withMessage("Organization name is invalid"),
+  //Sanitize fields
+  sanitizeBody("personalNumber")
+    .trim()
+    .escape(),
+  sanitizeBody("orgNumber")
+    .trim()
+    .escape(),
+  sanitizeBody("orgName")
+    .trim()
+    .escape(),
+  (req, res, next) => {
+    var token = req.roaring_access_token;
+    console.log(token);
+    var tasks = {
+      overview: function(callback) {
+        callRoaring(
+          callback,
+          "/se/company/overview/1.1/" + req.body.orgNumber,
+          "get",
+          undefined,
+          "COMPANY_OVERVIEW_INVALID_RESPONSE",
+          "COMPANY_OVERVIEW_API_ERROR",
+          token
+        );
+      },
+      ecoOverview: function(callback) {
+        callRoaring(
+          callback,
+          "/se/company/economy-overview/1.1/" + req.body.orgNumber,
+          "get",
+          undefined,
+          "COMPANY_ECOOVERVIEW_INVALID_RESPONSE",
+          "COMPANY_ECOOVERVIEW_API_ERROR",
+          token
+        );
+      },
+      boardMembers: function(callback) {
+        callRoaring(
+          callback,
+          "/se/company/board-members/1.1/" + req.body.orgNumber,
+          "get",
+          undefined,
+          "COMPANY_BOARDMEMBERS_INVALID_RESPONSE",
+          "COMPANY_BOARDMEMBERS_API_ERROR",
+          token
+        );
+      },
+      beneficialOwners: function(callback) {
+        callRoaring(
+          callback,
+          "/se/beneficialowner/1.0/company/" + req.body.orgNumber,
+          "get",
+          undefined,
+          "COMPANY_BENEFICIAL_INVALID_RESPONSE",
+          "COMPANY_BENEFICIAL_API_ERROR",
+          token
+        );
+      },
+      signatory: function(callback) {
+        callRoaring(
+          callback,
+          "/se/company/signatory/1.1/" + req.body.orgNumber,
+          "get",
+          undefined,
+          "COMPANY_SIGNATORY_INVALID_RESPONSE",
+          "COMPANY_SIGNATORY_API_ERROR",
+          token
+        );
+      },
+      cmpSanctionInfo: function(callback) {
+        callRoaring(
+          callback,
+          "/global/sanctions-lists/1.0/search",
+          "get",
+          {
+            name: req.body.orgName
+          },
+          "COMPANY_SANCTION_INVALID_RESPONSE",
+          "COMPANY_SANCTION_API_ERROR",
+          token
+        );
+      },
+      perSanctionInfo: function(callback) {
+        callRoaring(
+          callback,
+          "/global/sanctions-lists/1.0/search",
+          "get",
+          {
+            name: req.body.bankid.userInfo.name
+          },
+          "PERSON_SACNTION_INVALID_RESPONSE",
+          "PERSON_SACNTION_API_ERROR",
+          token
+        );
+      },
+      pepInfo: function(callback) {
+        callRoaring(
+          callback,
+          "/nordic/pep/1.0/search",
+          "get",
+          {
+            personalNumber: req.body.personalNumber,
+            countryCode: "se"
+          },
+          "PEP_INVALID_RESPONSE",
+          "PEP_API_ERROR",
+          token
+        );
       }
     };
-    //console.log(config);
-    axios(config)
-      .then(function(response) {
-        console.log(response.data);
-        console.log(response.status);
-        res.send(response.data);
-      })
-      .catch(function(error) {
-        if (error.response) {
-          // The request was made and the server responded with a status code
-          // that falls out of the range of 2xx
-          console.log(error.response.data);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-          res.status(error.response.status).send(error.response.data);
-        } else if (error.request) {
-          // The request was made but no response was received
-          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-          // http.ClientRequest in node.js
-          console.log(error.request);
-          res.status(204).send("No response from BankID server");
-        } else {
-          // Something happened in setting up the request that triggered an Error
-          console.log("Error", error.message);
-          res.status(500).send(error.message);
+    var roaring;
+    async.parallel(async.reflectAll(tasks), function(errors, results) {
+      console.log(errors);
+      //console.log(results);
+      for (var attr in results) req.body[attr] = results[attr].value;
+      token = req.sf_access_token;
+      console.log(req.body);
+      var apiRoot =
+        process.env.SALESFORCE_API_ROOT || "https://cs85.salesforce.com"; // for prod set to https://api.zignsec.com/v2
+      var config = {
+        url: "/services/apexrest/submitWithoutCallout",
+        baseURL: apiRoot,
+        method: "post",
+        data: req.body,
+        headers: {
+          Authorization: "Bearer " + token
         }
-        console.log(error.config);
-        res.status(400).send(error.config);
-      });
-  });
-};
+      };
+      //console.log(config);
+      axios(config)
+        .then(function(response) {
+          console.log(response.data);
+          console.log(response.status);
+          res.send(response.data);
+        })
+        .catch(function(error) {
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+            res.status(error.response.status).send(error.response.data);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            console.log(error.request);
+            res.status(204).send("No response from BankID server");
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+            res.status(500).send(error.message);
+          }
+          console.log(error.config);
+          res.status(400).send(error.config);
+        });
+    });
+  }
+];
 
 exports.getNeedsList = function(req, res, next) {
   var accessToken = req.access_token;
