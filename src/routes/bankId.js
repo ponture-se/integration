b242997f8b72fd9f04f204a62b98dac0f50e57a2
fakhrad <fakhrad@openratio.com>
2019-08-26 16:21:35 +0430

    login api added to auth routing

--- src/routes/bankId.js
+++ src/routes/bankId.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var controller = require("../controllers/bankIdController");
 var auth = require("../controllers/auth");
 
+router.post("/login", auth.noAuthNeeded, auth.getSalesForceToken, auth.login);
 router.post("/token", auth.noAuthNeeded, controller.authenticate);
 router.get("/collect", auth.verifyToken, controller.collect);
 router.post("/cancel", auth.verifyToken, controller.cancel);
