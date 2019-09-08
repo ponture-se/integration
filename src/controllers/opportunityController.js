996ebd852d7b9068a3f42ed9347498da68a72e2b
fakhrad <fakhrad@openratio.com>
2019-09-08 18:14:48 +0430

    empty companies list converted to [] output

--- src/controllers/opportunityController.js
+++ src/controllers/opportunityController.js
@@ -50,10 +50,26 @@ exports.getCompanies = [
               return x.statusCode == 100;
             });
             res.status(200).send(output);
-          } else res.status(404).send(response.data);
+          } else res.status(200).send([]);
         })
         .catch(function(error) {
-          res.send(error);
+          if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            res.status(error.response.status).send(error.response.data);
+          } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+            // http.ClientRequest in node.js
+            res.status(204).send("No response from Roaring server");
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+            res.status(500).send({ error: "Error in loading companioes" });
+          }
+          res
+            .status(400)
+            .send({ error: "Error in loading companioes from roaring" });
         });
     }
   }
