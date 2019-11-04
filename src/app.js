2fec75bf54c19706322f28be0c013958b25d4bfd
fakhrad <fakhrad@openratio.com>
2019-11-04 15:34:11 +0330

    account api route added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var cors = require('cors')
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var helmet = require('helmet');
-var compression = require('compression');
+var express = require("express");
+var cors = require("cors");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var helmet = require("helmet");
+var compression = require("compression");
 
 var app = express();
 
@@ -12,15 +12,17 @@ app.use(compression()); //Compress all routes
 app.use(helmet());
 app.use(cors());
 
-var bankid = require('./routes/bankId');
-var opportunity = require('./routes/opportunity');
+var bankid = require("./routes/bankId");
+var account = require("./routes/account");
+var opportunity = require("./routes/opportunity");
 // a middleware function with no mount path. This code is executed for every request to the router
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use("/auth", bankid);
+app.use("/accounts", bankid);
 app.use("/apply", opportunity);
 module.exports = app;
