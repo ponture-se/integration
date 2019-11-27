a0f6e9a9b7e686de634b65f21161af14b01d51e1
Abbas <abbas.farkoosh@gmail.com>
2019-11-27 14:22:54 +0330

    some roaring test are written

---
+++ tests/roaringTest.js
@@ -0,0 +1,32 @@
+const expect = require('chai').expect;
+const nock = require('nock');
+const roaringMockResponse = require('../mockResponses/roaringMockResponse');
+const roaring = require('../src/controllers/roaring');
+
+var apiRoot = process.env.ROARING_API_ROOT || "https://api.roaring.io";
+
+describe("Roaring Tests", () => {
+    // beforeEach(()=>{
+    //     nock(apiRoot)
+    //     .post('/token')
+    //     .reply(200, roaringMockResponse.token);
+    // });
+
+    it("getRoaringToken Test - OK", async () => {
+        nock(apiRoot)
+        .post('/token')
+        .reply(200, roaringMockResponse.token);
+
+        response = await roaring.getRoaringToken();
+        expect(response.success).to.equal(true);
+    });
+
+    it("getRoaringToken Test - unSuccess Status Code", async () => {
+        nock(apiRoot)
+        .post('/token')
+        .reply(500, roaringMockResponse.token);
+
+        response = await roaring.getRoaringToken();
+        expect(response.success).to.equal(false);
+    });
+});
