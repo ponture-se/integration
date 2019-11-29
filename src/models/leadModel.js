b5f200eef5ae230674eed4574a4f6b6968cff588
Abbas <abbas.farkoosh@gmail.com>
2019-11-29 17:17:35 +0330

    improve lead model

--- src/models/leadModel.js
+++ src/models/leadModel.js
@@ -38,11 +38,11 @@ const { body } = require('express-validator');
 
 function leadValidationRules() {
   return [
-    body('organization_number').isString().withMessage("Value Must be String.")
+    body('organization_number').isNumeric().withMessage("Value Must be Numeric.")
                               .exists().withMessage("Required Key/Value Pair")
                               .notEmpty().withMessage("Can not be Empty"),
-    body('phone').isString().withMessage("Value Must be String.").optional(),
-    body('email').isString().withMessage("Value Must be String.").optional(),
+    body('phone').isNumeric().withMessage("Value Must be Numeric.").optional(),
+    body('email').isEmail().withMessage("Value Must be Email.").optional(),
     body('situation').isString().withMessage("Value Must be String.")
                     .exists().withMessage("Required Key/Value Pair"),
     body('lead_revenue').isNumeric().withMessage("Value Must be Numeric.").optional(),
@@ -51,7 +51,7 @@ function leadValidationRules() {
                     .exists().withMessage("Required Key/Value Pair")
                     .notEmpty().withMessage("Can not be Empty"),
     body('first_name').isString().withMessage("Value Must be String.").optional(),
-    body('mobile').isString().withMessage("Value Must be String.").optional(),
+    body('mobile').isMobilePhone().withMessage("Value Must be Mobilephone.").optional(),
     body('problem').isArray().withMessage("Value Must be Array")
                             .isLength({ min: 1 }).withMessage("At least one value should exist")
                             .exists().withMessage("Required Key/Value Pair"),
@@ -68,8 +68,7 @@ function leadValidationRules() {
     body('last_referral_date').isString().withMessage("Value Must be String").optional(),
     body('lead_source').isString().withMessage("Value Must be String").optional(),
     body('lead_action').isString().withMessage("Value Must be String.").optional(),
-    body('specific_lead_source').isString().withMessage("Value Must be String").optional()
-    // body('description').isString().withMessage("Value Must be String.").exists().withMessage("Required Key/Value Pair"),
+    body('specific_lead_source').isURL().withMessage("Value Must be URL").optional()
   ];
 }
 
