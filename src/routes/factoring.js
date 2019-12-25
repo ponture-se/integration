878466e11d0b577261675f4b1d1c5972dce3e6d2
Abbas <abbas.farkoosh@gmail.com>
2019-12-25 12:41:21 +0330

    new "get" method added to "factoring" router

--- src/routes/factoring.js
+++ src/routes/factoring.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const factoringController = require('../controllers/factoringController');
 const validate = require("../middlewares/validate");
-const factoringValidationRules = require("../models/factoringModel");
+const factoringModel = require("../models/factoringModel");
 
 
 router.post('/submit', 
-            factoringValidationRules(), 
+            factoringModel.factoringSubmitValidationRules(),
             validate,
             factoringController.submitFactoring);
+
+
+router.get('/requests',
+            factoringModel.factoringRequestsValidation(),
+            validate,
+            factoringController.getCustomerFactoringApplications);
 
 
 module.exports = router;
