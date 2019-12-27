9a5327cee0a004c419937fbf38510e83d7a367e1
Abbas <abbas.farkoosh@gmail.com>
2019-12-27 11:15:43 +0330

    change factoring router to use new middlewares

--- src/routes/factoring.js
+++ src/routes/factoring.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const getSFConnection = require("../middlewares/sfMiddleware");
+const factoringMW = require("../middlewares/sfMiddlewares/factoringMW");
 const factoringController = require('../controllers/factoringController');
 const validate = require("../middlewares/validate");
 const factoringModel = require("../models/factoringModel");
 
 
-router.post('/submit', 
+router.post('/submit',
             factoringModel.factoringSubmitValidationRules(),
             validate,
+            getSFConnection,
+            factoringMW.getFacoringRecordTypeId,
             factoringController.submitFactoring);
 
 
 router.get('/requests',
             factoringModel.factoringRequestsValidation(),
             validate,
+            getSFConnection,
+            factoringMW.getFacoringRecordTypeId,
             factoringController.getCustomerFactoringApplications);
 
 router.get('/open',
             factoringModel.factoringOpenDetailsValidation(),
             validate,
+            getSFConnection,
+            factoringMW.getFacoringRecordTypeId,
             factoringController.openFactoringOpp);
+
+router.put('/cancelApplication',
+            factoringModel.factoringCancelAppValidation(),
+            validate,
+            getSFConnection,
+            factoringMW.getFacoringRecordTypeId,
+            factoringController.cancelFactoringApp);
 
 module.exports = router;
