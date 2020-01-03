eccbdc121845f0a39dc854f3d8c16c001d10abb5
Abbas <abbas.farkoosh@gmail.com>
2020-01-03 12:15:07 +0330

    "updateSobjectInSf" function added

--- src/controllers/sfHelpers/crudHelper.js
+++ src/controllers/sfHelpers/crudHelper.js
@@ -10,6 +10,23 @@ async function insertSobjectInSf(sfConn, sObj, payload) {
 }
 
 
+async function updateSobjectInSf(sfConn, sObj, payload, existingId = null) {
+  if (!payload.hasOwnProperty('Id') && existingId != null) {
+    payload['Id'] = existingId;
+  }
+
+  try {
+    const result = await sfConn.sobject(sObj)
+                                .update(payload);
+    return result;
+  } catch (err) {
+    console.error('Error in updateSobjectInSf (' + sObj + ')', err);
+    return null;
+  }
+}
+
+
 module.exports = {
     insertSobjectInSf,
+    updateSobjectInSf
 }
