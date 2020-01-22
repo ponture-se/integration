e97cf6f18f19572fc3b563cbcd44242052665281
Abbas <abbas.farkoosh@gmail.com>
2020-01-22 18:26:26 +0330

    prepare middlewares for files

--- src/middlewares/sfMiddlewares/fileMW.js
+++ src/middlewares/sfMiddlewares/fileMW.js
@@ -1,6 +1,11 @@
 const fileController = require('../../controllers/fileController');
 const myResponse = require('../../controllers/myResponse');
 const apiLogger = require('../apiLogger');
+const logger = require('../../controllers/customeLogger');
+const multer = require('multer');
+const dotenv = require('dotenv');
+dotenv.config();
+
 
 
 async function uploadFile(req, res, next) {
@@ -43,6 +48,59 @@ async function uploadFile(req, res, next) {
 }
 
 
+
+function uploadFileExtraValidation(req, res, next){
+    let resBody,
+        validationError = false;
+
+    if (!req.file) {
+        resBody = myResponse(false, null, 400, 'No file attached');
+        res.status(400).send(resBody);
+
+        validationError = true;
+    }
+
+    if (validationError) {
+        res.body = resBody;			// For logging purpose
+        return apiLogger(req, res, () => {return;});			//instead of calling next()
+    } else {
+        return next();
+    }
+}
+
+
+function uploadErrorHandler(err, req, res, next) {
+    let resBody;
+    if (err instanceof multer.MulterError) {
+        resBody = myResponse(false, null, 500, err.message);
+        res.status(500).send(resBody);
+
+        res.body = resBody;
+        return apiLogger(req, res, () => {return;});			//instead of calling next()
+    } else if (err) {
+        logger.error('File ERROR', {metadata: err});
+        resBody = myResponse(false, null, 500, 'Something went wrong', err.message);
+        res.status(500).send(resBody);
+
+        res.body = resBody;
+        return apiLogger(req, res, () => {return;});			//instead of calling next()
+    } else {
+        next();
+    }
+}
+
+function fileFilter (req, file, callback) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|pdf|txt|doc|docx|ppt|pptx|xls|xlsx)$/)) {
+      return callback(new Error('File type is Invalid!'), false);
+    }
+
+    callback(null, true);
+  };
+
+
 module.exports = {
-    uploadFile
+    uploadFile,
+    uploadFileExtraValidation,
+    uploadErrorHandler,
+    fileFilter
 }
