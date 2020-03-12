26637a5a098b43d12f5cd0a1461d34c679df1b9e
Abbas <abbas.farkoosh@gmail.com>
2020-03-12 18:00:44 +0330

    add router for "getPartnersForMatchMake"

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,17 +2,26 @@ const express = require('express');
 const router = express.Router();
 const userMW = require('../middlewares/sfMiddlewares/userMW');
 const validate = require("../middlewares/validate");
-const userModel = require("../models/userModel");
+const userValidationRules = require("../models/userModel");
+const getSFConnection = require("../middlewares/sfMiddleware");
 var auth = require("../controllers/auth");
+
 
 
 router.post('/login',
             auth.noAuthNeeded,
-            userModel.loginValidation(),
+            userValidationRules.loginValidation(),
             validate,
             auth.getSalesForceToken,
             userMW.loginApi);
-
+            
+            
+router.get('/getPartnersForMatchMake',
+            auth.verifyToken,
+            userValidationRules.getPartnerForMatchMakeValidation(),
+            validate,
+            getSFConnection,
+            userMW.getPartnerForMatchMakeAPI);
 
 
 module.exports = router;
