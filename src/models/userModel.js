1689efc27dd15c0f5ce838799eb78e911d1f7e23
Abbas <abbas.farkoosh@gmail.com>
2020-03-13 17:57:28 +0330

    "manualMatchMakingValidation" user model added

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,11 +16,29 @@ function loginValidation(){
 function getPartnerForMatchMakeValidation() {
 	return [
 		query('oppId').isString().withMessage('It Should be String')
-						.exists().withMessage("Required Key/Value Pair")
+            .exists().withMessage("Required Key/Value Pair")
+            .trim()
             .notEmpty().withMessage("Can not be Empty")
+	];
+}
+
+
+function manualMatchMakingValidation() {
+	return [
+		body('opp_id').isString().withMessage('It Should be String')
+            .exists().withMessage("Required Key/Value Pair")
+            .trim()
+            .notEmpty().withMessage("Can not be Empty"),
+    body('partners_id').exists().withMessage("Required Key/Value Pair")
+                      .isLength({
+                        min: 1
+                      }).withMessage("At least one value should exist")
+                      .isArray().withMessage("Value Must be Array")
+
 	];
 }
   module.exports = {
 	loginValidation,
-	getPartnerForMatchMakeValidation
+  getPartnerForMatchMakeValidation,
+  manualMatchMakingValidation
   }
