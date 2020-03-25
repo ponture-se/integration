921fefea82b030e52d3f65fc9f4c8f131b4a9d3c
Abbas <abbas.farkoosh@gmail.com>
2020-03-25 17:51:24 +0430

    manualMatchMakingValidation changed

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -29,10 +29,9 @@ function manualMatchMakingValidation() {
             .exists().withMessage("Required Key/Value Pair")
             .trim()
             .notEmpty().withMessage("Can not be Empty"),
-    body('partners_id').exists().withMessage("Required Key/Value Pair")
-                      .isLength({
-                        min: 1
-                      }).withMessage("At least one value should exist")
+    body('assign').exists().withMessage("Required Key/Value Pair")
+                      .isArray().withMessage("Value Must be Array"),
+    body('unassign').exists().withMessage("Required Key/Value Pair")
                       .isArray().withMessage("Value Must be Array"),
     body('with_submit').isBoolean().withMessage('It Should be boolean')
                   .isIn([true, false]).withMessage("Valid Values are: true, false")
