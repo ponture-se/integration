7c58030edbc913b8c17124dfec3fd9d1f5b7624c
Abbas <abbas.farkoosh@gmail.com>
2020-03-25 11:48:34 +0430

    "with_submit" added to model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -33,7 +33,10 @@ function manualMatchMakingValidation() {
                       .isLength({
                         min: 1
                       }).withMessage("At least one value should exist")
-                      .isArray().withMessage("Value Must be Array")
+                      .isArray().withMessage("Value Must be Array"),
+    body('with_submit').isBoolean().withMessage('It Should be boolean')
+                  .exists().withMessage("Required Key/Value Pair")
+                  .notEmpty().withMessage("Can not be Empty"),
 
 	];
 }
