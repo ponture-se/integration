986c92ceb641e861f61f3ef67c279c98e8dd42ea
Abbas <abbas.farkoosh@gmail.com>
2020-04-20 11:18:27 +0430

    revise "setTagForOffersList" and add different functions for each tag

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,48 +1,95 @@
 const _ = require('lodash');
 
 function setTagForOffersList(offerList) {
-    let cheepestOfferValue = Infinity,
-        cheepestOfferId = '',
-        mostExpensiveOfferValue = -Infinity,
-        mostExpensiveOfferId = '';
+
+    // sort by date, to tag equal values based on eralier createdDate
+    offerList = _.sortBy(offerList, ['CreatedDate'], ['asc']);
+
+    // Tag Lists
+    offerList = tag_addCheapestTagForOffersList(offerList);
+    offerList = tag_addBiggestTagForOffersList(offerList);
+
+    // convert list to comma-seperated
+    offerList.forEach(o => {
+        let tag = _.get(o, 'tag', []);
+
+        o.tag = tag.join(',');
+    });
+
+    // resort offerList to show in descending order by CreatedDate
+    offerList = _.sortBy(offerList, ['CreatedDate'], ['desc']);
+
+    return offerList;
+
+}
+
+function tag_addCheapestTagForOffersList(offerList) {
+    let cheapestOfferValue = Infinity,
+        cheapestOfferId = '';
 
     // calculate to find value
     offerList.forEach(o => {
-        let amount = _.get(o, 'Amount');
-        let loanPeriod = _.get(o, 'Loan_Period');
+        let monthlyRepayment = _.get(o, 'detail.Monthly_Repayment');
 
-        if (amount && loanPeriod ){
-            let rate = amount / loanPeriod;
-
-            if (rate > mostExpensiveOfferValue) {
-                mostExpensiveOfferValue = rate;
-                mostExpensiveOfferId = _.get(o, 'Id', '__');
-            }
-
-            if (rate < cheepestOfferValue) {
-                cheepestOfferValue = rate;
-                cheepestOfferId = _.get(o, 'Id', '__');
+        if (monthlyRepayment){
+            if (monthlyRepayment < cheapestOfferValue) {
+                cheapestOfferValue = monthlyRepayment;
+                cheapestOfferId = _.get(o, 'Id', '__');
             }
         }
     });
 
     // Set tag Fields
     offerList.forEach(o => {
-        let tag = [];
+        let tag = _.get(o, 'tag', []);
         
         let offerId = _.get(o, 'Id');
-        if (offerId && offerId == cheepestOfferId) {
-            tag.push('cheepest');
-        }
-        if (offerId && offerId == mostExpensiveOfferId) {
-            tag.push('biggest');
+        if (offerId && offerId == cheapestOfferId) {
+            tag.push('cheapest');
         }
 
-        o.tag = tag.join(',');
+        o.tag = tag;
     });
 
     return offerList;
+}
 
+function tag_addBiggestTagForOffersList(offerList) {
+    let biggestAmountOfferValue = -Infinity,
+        biggestPeriodOfferValue = -Infinity,
+        biggestOfferId = '';
+
+    // calculate to find value
+    offerList.forEach(o => {
+        let amount = _.get(o, 'Amount');
+        let period = _.get(o, 'Loan_Period');
+
+        if (amount){
+            if (amount > biggestAmountOfferValue) {
+                biggestAmountOfferValue = amount;
+                biggestPeriodOfferValue = period;
+                biggestOfferId = _.get(o, 'Id', '__');
+            } else if (amount == biggestAmountOfferValue && period > biggestPeriodOfferValue) {
+                biggestAmountOfferValue = amount;
+                biggestPeriodOfferValue = period;
+                biggestOfferId = _.get(o, 'Id', '__');
+            }
+        }
+    });
+
+    // Set tag Fields
+    offerList.forEach(o => {
+        let tag = _.get(o, 'tag', []);
+        
+        let offerId = _.get(o, 'Id');
+        if (offerId && offerId == biggestOfferId) {
+            tag.push('biggest');
+        }
+
+        o.tag = tag;
+    });
+
+    return offerList;
 }
 
 
