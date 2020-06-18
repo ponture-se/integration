340ca21af3ff59f30c634ca43b5077b2b68aa7c4
Abbas <abbas.farkoosh@gmail.com>
2020-06-18 16:17:53 +0430

    some constants added

--- src/controllers/Constants.js
+++ src/controllers/Constants.js
@@ -1,4 +1,13 @@
 module.exports = {
-    MIN_AMOUNT_FOR_BANKID_BYPASS: 2000000,
-    OPP_STAGE_OF_OPP_CREATION_WITHOUT_BANK_ID: 'App Review'
+    MIN_AMOUNT_FOR_BANKID_BYPASS: 1999999,
+    MIN_AMOUNT_FOR_NON_GENERAL_NEED_TO_BANKID_BYPASS: 999999,
+    MIN_AMOUNT_FOR_AB_COMPANY_TO_BANKID_BYPASS: 999999,
+    MIN_TURNOVER_FOR_AB_COMPANY_TO_BANKID_BYPASS: 20000000,
+    OPP_STAGE_OF_OPP_CREATION_WITHOUT_BANK_ID: 'App Review',
+    NON_GENERAL_LIQUIDITY_NEEDS: [
+        'purchase_of_real_estate',
+        'construction_project',
+        'loan_on_real_estate',
+        'purchase_of_business'
+    ]
 }
